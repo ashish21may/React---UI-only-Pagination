9b135ad0f5a03fc6fd902c3e5724e7caf2537ba3
Ashish <apathak@altimetrik.com>
2022-03-11 00:35:34 +0530

    Post Component code - refactor

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,54 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import '../App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css";
 
-function Posts({handleTotalPages, currentPageNumber = 1}) {
-
-  console.log('currentPageNumber', currentPageNumber)
-  // let noPostMessage = useRef();
+function Posts({ handleTotalPages, currentPageNumber = 1 }) {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [totalPages, setTotalPages] = useState(1);
-  // const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
-  const [paginatedPostData, setPaginatedPostData] = useState([])
-
+  const [paginatedPostData, setPaginatedPostData] = useState([]);
 
   useEffect(() => {
     const getPostData = async () => {
       try {
-        setLoading(true)
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setLoading(true);
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
         setPosts(response.data);
-        setTotalPages(Math.ceil(response?.data?.length/10));
-        handleTotalPages(Math.ceil(response?.data?.length/10));
+        setTotalPages(Math.ceil(response?.data?.length / 10));
+        handleTotalPages(Math.ceil(response?.data?.length / 10));
       } catch {
-        // noPostMessage.current.value = 'No Data available for posts.';
+        // Error handling can be done here
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
     getPostData();
-  }, [])
-
-  // useEffect(())
+  }, []);
 
   useEffect(() => {
-    if(posts.length) {
+    if (posts.length) {
       const postsToShow = JSON.parse(JSON.stringify(posts));
-      // const lastIndex = Math.min((postsPerPage * currentPage) - 1, posts.length-1);
-      const startIndex = (currentPageNumber * postsPerPage) - postsPerPage;
-      const post = postsToShow.splice(startIndex, currentPageNumber === totalPages ? posts.length - startIndex : postsPerPage)
-      setPaginatedPostData(post)
+      const startIndex = currentPageNumber * postsPerPage - postsPerPage;
+      const post = postsToShow.splice(
+        startIndex,
+        currentPageNumber === totalPages
+          ? posts.length - startIndex
+          : postsPerPage
+      );
+      setPaginatedPostData(post);
     }
-  }, [posts, currentPageNumber])
-  // 
+  }, [posts, currentPageNumber]);
+  //
   return (
     <div>
-      {loading && <p> Loading ... </p>}
-      {!loading && <ul>{paginatedPostData.map(post => <li className="single-post-style" key={post.id}>{post.body} </li>)}</ul>}
+      {loading && <p className="center-text"> Loading ... </p>}
+      {!loading && (
+        <ul>
+          {paginatedPostData.map((post) => (
+            <li className="single-post-style" key={post.id}>
+              {post.body}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
